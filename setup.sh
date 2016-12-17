df4894e1605e40355dff36ed3fec89090a29d24c
Srinath Janakiraman <me@vjsrinath.com>
2016-12-17 20:43:30 +0530

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
+git clone https://github.com/torch/distro.git ~/torch --recursive
+cd ~/torch; bash install-deps;
+./install.sh
+
+source ~/.bashrc
+
+for NAME in dpnn nn optim optnet csvigo cutorch cunn fblualib torchx tds; do luarocks install $NAME; done
+
 ./models/get-models.sh
 pip2 install -r requirements.txt
 python2 setup.py install
