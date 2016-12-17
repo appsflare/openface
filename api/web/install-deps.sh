2e333592137dc51bc29c619975c4049f1291762f
Srinath Janakiraman <me@vjsrinath.com>
2016-12-17 16:56:23 +0530

    renamed demos to api

--- api/web/install-deps.sh
+++ api/web/install-deps.sh
@@ -2,13 +2,19 @@
 
 set -x -e
 
+source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
+wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
+
+
+
 sudo apt-get update
 sudo apt-get install -y libprotobuf-dev libleveldb-dev libsnappy-dev \
   libopencv-dev libhdf5-serial-dev libboost-all-dev libgflags-dev \
   libgoogle-glog-dev liblmdb-dev protobuf-compiler libboost-all-dev \
   libatlas-dev libatlas-base-dev liblapack-dev libblas-dev \
   python-pip python-numpy python-imaging python-opencv \
-  git wget cmake gfortran
+  git wget cmake gfortran \
+  rethinkdb
 
 mkdir -p ~/src
 cd ~/src
